eb1774ee44df3e63c1f8e590541da827d6d5b808
Kamini Kamal <kaminikamaliem@gmail.com>
2017-02-02 16:52:46 +0530

    mod

--- middlewares.js
+++ middlewares.js
@@ -3,7 +3,7 @@ module.exports = {
 	if(req.session.name==undefined){
 		console.log("user signed out");
 res.redirect('/login');
-		//res.render('login', {title : "please log in into your account"});
+
 	}
 	next();
 }
